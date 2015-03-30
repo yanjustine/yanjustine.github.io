112949b60a689cfa485b7841efd6dd08a44b155d
ptsteadman <ptsteadman@gmail.com>
2015-03-30 01:06:06 -0400

    video

--- assets/app.js
+++ assets/app.js
@@ -17,16 +17,21 @@ $(document).ready(function(){
 			var project = "";
 			project += "<h5 id='" + side + "-back'><a>Back</a></h5>";
 			project += "<h4 class='project'>" + title+ "</h4>";
-			project += "<div class='image'><img src='" + content[side][title][index]["src"] + "'></div>";
+
+			project += "<h4 class='project'>" + title+ "</h4>";
+			var type = content[side][title][index]["type"];
+			if(type == "video"){
+				project += "<iframe width='560' height='315' src='" + content[side][title][index]["src"]  + "?showinfo=0&modestbranding=1&vq=hd1080' frameborder='0' allowfullscreen></iframe>";
+			} else {
+				// image
+				project += "<div class='image'><img src='" + content[side][title][index]["src"] + "'></div>";
+			}
 			project += "<h5 class='caption'>" + content[side][title][index]["caption"] + "<h5>";
 			$("#" + side).html(project);
-			$(".image img").click(function(e){
-				var side = $(e.target).parent().parent().attr('id');
-				app.showProject(side, title, (index + 1) % content[side][title].length);
-			});
 			$("#" +side + "-back").click(function(){
 				app.showContents(side);
 			});
+
 		},
 
 		init: function(){
