64623905d59d9788394e83ba36812a548fcbb5de
ptsteadman <ptsteadman@gmail.com>
2015-03-30 00:08:44 -0400

    working

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
-	for(var title in content["left"]){
-		$("#left").append("<h3><a>" + title + "</a></h3>");
-	}
 
-	for(var title in content["right"]){
-		$("#right").append("<h3><a>" + title + "</a></h3>");
-	}
+	var app = {
+
+		showContents: function(side){
+			$("#" + side).html("");
+			for(var title in content[side]){
+				$("#" + side).append("<h3><a class='title'>" + title + "</a></h3>");
+			}
+		},
+
+		showProject: function(side, title, index){
+			var project = "";
+			project += "<h5 id='" + side + "-back'><a>Back</a></h5>";
+			project += "<h4 class='project'>" + title+ "</h4>";
+			project += "<img class='image' src='" + content[side][title][index]["src"] + "'>";
+			project += "<h5 class='caption'>" + content[side][title][index]["caption"] + "<h5>";
+			$("#" + side).html(project);
+			$(".image").click(function(e){
+				var side = $(e.target).parent().attr('id');
+				app.showProject(side, title, (index + 1) % content[side][title].length);
+			});
+			$("#" +side + "-back").click(function(){
+				app.showContents(side);
+			});
+		},
+
+		init: function(){
+			app.showContents("left");
+			app.showContents("right");
+			$(".title").click(function(e){
+				var side = $(e.target).parent().parent().attr('id');
+				app.showProject(side, $(e.target).text(), 0);
+			});
+
+		}
+	};
+
+
+	app.init();
 
 });
